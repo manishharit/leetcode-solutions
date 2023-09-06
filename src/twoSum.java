4355c85aab30d5be08ebc49ebaf0092816ca2ed6
manishharit <manishharit974@gmail.com>
2023-09-06 19:08:29 +0530

    Two Sum

---
+++ src/twoSum.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum {
+    public static void main(String[] args) {
+        twoSum(new int[]{2,7,11,15},9);
+        }
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> data = new HashMap<>();
+
+        for(int i=0; i< nums.length;i++){
+            int value = target - nums[i];
+
+            if(data.containsKey(value)){
+                System.out.println(Arrays.toString(new int[]{data.get(value), i}));
+                return new int[]{data.get(value),i};
+            }else {
+                data.put(nums[i],i);
+            }
+        }
+        return null;
+
+    }
+
+}
